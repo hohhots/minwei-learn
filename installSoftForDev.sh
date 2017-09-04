8ee335423c749bda1bdb3f1c437e579dc8fb61d3
brgd <hohhots@gmail.com>
2017-09-04 23:06:21 +0800

    add install shell

---
+++ installSoftForDev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#sudo apt-get update && time sudo apt-get upgrade
+
+COMMANDS=( 'nodejs') # 'node' 'git' )
+COMMANDSPACKAGES=( 'nodejs' 'nodejs-legacy' 'git' )
+comNum=${#COMMANDS[@]}
+for(( i=0;i<$comNum;i++)); do
+    echo "Checking if command \"${COMMANDS[${i}]}\" exists! It's package name is ${COMMANDSPACKAGES[${i}]}"
+    if [ ! -z `which ${COMMANDS[${i}]}` ]  #if command exist!
+    then
+        echo "${COMMANDS[${i}]} already installed!"
+        sudo apt-get remove --purge ${COMMANDSPACKAGES[${i}]}
+    fi
+        sudo apt-get install -y ${COMMANDSPACKAGES[${i}]}
+        echo "${COMMANDS[${i}]} installed!"
+    
+done
+
+#fab setup
