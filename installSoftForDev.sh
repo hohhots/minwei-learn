543f5134f73c966e623513cdb9f6294e31ff2aca
brgd <hohhots@gmail.com>
2017-09-05 00:10:48 +0800

    add install fabric

--- installSoftForDev.sh
+++ installSoftForDev.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 
-COMMANDS=( 'nodejs' 'node' 'git' )
+COMMANDS=( 'nodejs' 'node' 'fab' )
 
-COMMANDSPACKAGES=( 'nodejs' 'nodejs-legacy' 'git' )
+COMMANDSPACKAGES=( 'nodejs' 'nodejs-legacy' 'fabric' )
 
 comNum=${#COMMANDS[@]}
 for(( i=0;i<$comNum;i++)); do
     echo "Checking if command \"${COMMANDS[${i}]}\" exists! It's package name is ${COMMANDSPACKAGES[${i}]}"
     if [ ! -z `which ${COMMANDS[${i}]}` ]  #if command exist!
     then
-        echo "${COMMANDS[${i}]} already installed!"
+        echo "${COMMANDS[${i}]} already installed, and will be REMOVED!!!!"
         sudo apt-get remove -y --purge ${COMMANDSPACKAGES[${i}]}
     fi
-        sudo apt-get install -y ${COMMANDSPACKAGES[${i}]}
-        echo "${COMMANDS[${i}]} installed!"
+    
+    sudo apt-get install -y ${COMMANDSPACKAGES[${i}]}
+    echo "${COMMANDS[${i}]} installed!"
     
 done
 
